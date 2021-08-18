82f22dbfa5a006a810b5302970aa5795921a10b9
Vio <53267372+vio-codes@users.noreply.github.com>
2021-08-18 11:22:44 +0300

    Update painterly_rendering.py

--- apps/painterly_rendering.py
+++ apps/painterly_rendering.py
@@ -36,8 +36,6 @@ def main(args):
     num_paths = args.num_paths
     max_width = args.max_width
     
-    random.seed(1234)
-    torch.manual_seed(1234)
     
     shapes = []
     shape_groups = []
