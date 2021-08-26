844c720a002fc8126aaad0c8122987cebe77ff56
vio-codes <chereches.andrei@hotmail.com>
2021-08-26 18:01:34 +0000

    try dice loss

--- apps/painterly_rendering_clip.py
+++ apps/painterly_rendering_clip.py
@@ -148,12 +148,12 @@ def main(args):
         smooth = 1
         intersection = (image_features * text_features).sum()                            
         dice = (2.*intersection + smooth)/(image_features.sum() + text_features.sum() + smooth)  
-        dice_loss =  (1 - dice)*100
+        dice_loss =  1 - dice
         #Cos loss
         cos_loss = (torch.cos(image_features) - torch.cos(text_features)).pow(2).mean()*100
         #loss = -torch.cosine_similarity(text_features, image_features, dim=-1).mean()
-        loss = dice_loss + cos_loss
-        print('render loss:', loss.item(),"dice:",dice_loss.item(),"cos:",cos_loss)
+        loss = dice_loss 
+        print('render loss:', loss.item(),"dice:",dice_loss.item(),"cos:",cos_loss.item())
         # Backpropagate the gradients.
         loss.backward()
 
