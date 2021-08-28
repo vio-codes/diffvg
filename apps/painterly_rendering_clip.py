50a481d55cfdc2b6d4aad54c1dfe9c7847c0250e
vio-codes <chereches.andrei@hotmail.com>
2021-08-28 09:34:44 +0000

    triple loss

--- apps/painterly_rendering_clip.py
+++ apps/painterly_rendering_clip.py
@@ -40,7 +40,7 @@ def cos_loss(inputs, targets, y = 1):
     return cos_loss
 
 def triple_loss(inputs ,positives, negatives):
-    triplet_loss =  nn.TripletMarginWithDistanceLoss()
+    triplet_loss =  nn.TripletMarginWithDistanceLoss(distance_function=nn.CosineSimilarity(dim=-1))
     output = triplet_loss(inputs, positives, negatives)
     return output
 
