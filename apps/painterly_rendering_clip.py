ec341708978b1bba36a23da5887304e9b9c88d85
vio-codes <chereches.andrei@hotmail.com>
2021-08-28 14:29:57 +0000

    added colab display

--- apps/painterly_rendering_clip.py
+++ apps/painterly_rendering_clip.py
@@ -142,7 +142,7 @@ def main(args):
     pydiffvg.set_use_gpu(torch.cuda.is_available())
     augment_trans = transforms.Compose([ 
     transforms.RandomResizedCrop(224, scale=(0.7,0.9), ratio=(9/16, 16/9)),
-    transforms.RandomPerspective(fill=1, p=0.5, distortion_scale=0.5),
+    transforms.RandomAffine(),
     transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
     ])
     resize_aug = transforms.Resize(224)
