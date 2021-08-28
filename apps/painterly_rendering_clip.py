563cba3dfabfa338e90fb231f4369fe73fa093b7
vio-codes <chereches.andrei@hotmail.com>
2021-08-28 06:14:27 +0000

    m

--- apps/painterly_rendering_clip.py
+++ apps/painterly_rendering_clip.py
@@ -179,13 +179,13 @@ def main(args):
         canvas_width, canvas_height, shapes, shape_groups)
     render = pydiffvg.RenderFunction.apply
     # Optimize
-    gamma =0.6
+    gamma =0.9
     step_size =100
-    points_optim = torch.optim.Adam(points_vars, lr=10.0)
-    color_optim = torch.optim.Adam(color_vars, lr=1.0)
-    begin_optim = torch.optim.Adam(begin_vars, lr=1.0)
-    end_optim = torch.optim.Adam(end_vars, lr=1.0)
-    offsets_optim = torch.optim.Adam(offsets_vars, lr=1.0)
+    points_optim = torch.optim.Adam(points_vars, lr=5.0)
+    color_optim = torch.optim.Adam(color_vars, lr=0.1)
+    begin_optim = torch.optim.Adam(begin_vars, lr=0.1)
+    end_optim = torch.optim.Adam(end_vars, lr=0.1)
+    offsets_optim = torch.optim.Adam(offsets_vars, lr=0.1)
     schedulers =[]
     points_scheduler = torch.optim.lr_scheduler.StepLR(points_optim, step_size=step_size, gamma=gamma)
     schedulers.append(points_scheduler)
