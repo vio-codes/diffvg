b3704bc67f75188922e21e55b00eb2eb4f5549cb
Vio <53267372+vio-codes@users.noreply.github.com>
2021-08-29 14:52:53 +0300

    y

--- apps/painterly_rendering_clip.py
+++ apps/painterly_rendering_clip.py
@@ -143,9 +143,11 @@ def main(args):
 
     augment_trans = transforms.Compose([ 
     #transforms.RandomResizedCrop(224, scale=(0.7,0.9), ratio=(9/16, 16/9)),
+    transforms.CenterCrop(200),
     transforms.RandomAffine(degrees=(0, 180), translate=(0.5, 0.5), scale=(0.7, 0.9), fill= 1),
     transforms.RandomPerspective(fill=1, p=1, distortion_scale=0.5),
     transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.2),
+    transform.Resize(224),
     transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
     ])
 
