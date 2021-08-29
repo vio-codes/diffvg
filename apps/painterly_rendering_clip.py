fc092ae59c99f6806e5925eb883aac7b8d16e3cd
Vio <53267372+vio-codes@users.noreply.github.com>
2021-08-29 17:01:13 +0300

    y

--- apps/painterly_rendering_clip.py
+++ apps/painterly_rendering_clip.py
@@ -189,10 +189,10 @@ def main(args):
     # Optimize
 
     #points_optim = torch.optim.Adam(points_vars, lr=2.0)
-    color_optim = torch.optim.Adam(color_vars, lr=0.2)
-    begin_optim = torch.optim.Adam(begin_vars, lr=0.5)
-    end_optim = torch.optim.Adam(end_vars, lr=0.5)
-    offsets_optim = torch.optim.Adam(offsets_vars, lr=0.2)
+    color_optim = torch.optim.Adam(color_vars, lr=0.02)
+    begin_optim = torch.optim.Adam(begin_vars, lr=0.05)
+    end_optim = torch.optim.Adam(end_vars, lr=0.05)
+    offsets_optim = torch.optim.Adam(offsets_vars, lr=0.02)
     # Adam iterations.
 
     for t in range(args.num_iter):
