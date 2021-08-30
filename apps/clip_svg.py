3bc5f4ca812f78dcc695651b101f40b57ad3ba06
Vio <53267372+vio-codes@users.noreply.github.com>
2021-08-30 15:23:13 +0300

    y

--- apps/clip_svg.py
+++ apps/clip_svg.py
@@ -221,7 +221,7 @@ def main(args):
             image_features.append(clip_utils.simple_img_embed(aug))
         #Loss compared to original image
         for poz_text_feature in poz_text_features:
-                loss+= cos_loss(img_org_feature, poz_text_feature) + spherical_dist_loss(image_feature, poz_text_feature)
+                loss+= cos_loss(img_org_feature, poz_text_feature) + spherical_dist_loss(img_org_feature, poz_text_feature)
         #Loss compared to augmetations
         if args.augment:
             for _ in range(NUM_AUGS):
