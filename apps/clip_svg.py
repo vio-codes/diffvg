a7d52de45528b29665be80215dff2e102bac8df9
Vio <53267372+vio-codes@users.noreply.github.com>
2021-08-30 14:12:14 +0300

    y

--- apps/clip_svg.py
+++ apps/clip_svg.py
@@ -3,7 +3,6 @@ import torch
 import skimage
 import ttools.modules
 import skimage.io
-from skimage.io import imshow
 import skimage.color
 import random
 import argparse
@@ -225,7 +224,7 @@ def main(args):
                 img_augs.append(augment_trans(img))
             for aug in img_augs:
                 image_features.append(clip_utils.simple_img_embed(aug))
-                pydiffvg.imwrite(aug.cpu(), '/content/results/clip_svg/iter_{}aug{}.png'.format(t,img_augs.index(aug)), gamma=gamma)
+                pydiffvg.imwrite(aug.cpu(), '/content/results/clip_svg/iter_{}aug{}.png'.format(t,img_augs.index(aug[0])), gamma=gamma)
             for image_feature in image_features:
                 for poz_text_feature in poz_text_features:
                         loss+= cos_loss(image_feature, poz_text_feature)/NUM_AUGS
