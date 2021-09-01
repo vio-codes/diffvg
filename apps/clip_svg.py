ed0caa669ae031cf23255e46e0c0416618e93f5a
Vio <53267372+vio-codes@users.noreply.github.com>
2021-09-01 18:50:00 +0300

    y

--- apps/clip_svg.py
+++ apps/clip_svg.py
@@ -300,10 +300,10 @@ def main(args):
     # Optimize
 
     points_optim = torch.optim.Adam(points_vars, lr=2.0)
-    color_optim = torch.optim.Adam(color_vars, lr=0.5)
-    begin_optim = torch.optim.Adam(begin_vars, lr=0.5)
-    end_optim = torch.optim.Adam(end_vars, lr=0.5)
-    offsets_optim = torch.optim.Adam(offsets_vars, lr=0.5)
+    color_optim = torch.optim.Adam(color_vars, lr=0.1)
+    begin_optim = torch.optim.Adam(begin_vars, lr=0.1)
+    end_optim = torch.optim.Adam(end_vars, lr=0.1)
+    offsets_optim = torch.optim.Adam(offsets_vars, lr=0.1)
     # Adam iterations.
 
     for t in range(args.num_iter):
