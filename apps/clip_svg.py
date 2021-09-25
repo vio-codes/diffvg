afced4b6413cc5b66d699da7ab59c3e54e2a9222
Vio <53267372+vio-codes@users.noreply.github.com>
2021-09-25 16:22:11 +0300

    y

--- apps/clip_svg.py
+++ apps/clip_svg.py
@@ -343,7 +343,7 @@ def main(args):
                 image_features.append(clip_utils.simple_img_embed(aug))
             for image_feature in image_features:
                 for poz_text_feature in poz_text_features:
-                        loss+= (cos_loss(image_feature, poz_text_feature) + spherical_dist_loss(image_feature, poz_text_feature))
+                        loss+= (cos_loss(image_feature, poz_text_feature) + spherical_dist_loss(image_feature, poz_text_feature))/NUM_AUGS
 
 
         print('render loss:', loss.item())
