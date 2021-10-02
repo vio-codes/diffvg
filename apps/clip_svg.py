1ff0b3e0bdc777edd3569e21fa1e8391bc454f6f
Vio <53267372+vio-codes@users.noreply.github.com>
2021-10-02 12:14:46 +0300

    y

--- apps/clip_svg.py
+++ apps/clip_svg.py
@@ -283,7 +283,7 @@ def main(args):
     render = pydiffvg.RenderFunction.apply
     # Optimize
 
-    points_optim = torch.optim.Adam(points_vars, lr=1.0)
+    points_optim = torch.optim.Adam(points_vars, lr=2.0)
     color_optim = torch.optim.Adam(color_vars, lr=0.1)
     begin_optim = torch.optim.Adam(begin_vars, lr=0.1)
     end_optim = torch.optim.Adam(end_vars, lr=0.1)
